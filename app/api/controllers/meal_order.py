755e6cadcd2317036704ac1dee2744cbe5e965e3
leensungzero <brianwoo119@naver.com>
2019-05-23 09:26:10 +0900

    MealOrder controller response 정리
    app/api/controllers/meal_order.py
     - MOD: make_response를 이용하여 명시적인 return_data와 status_code 제공

--- app/api/controllers/meal_order.py
+++ app/api/controllers/meal_order.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response
 
 from app import db
 from app.api.models.meal import MealModel
@@ -22,18 +22,42 @@ def update_meal_order():
             feed_order.insert(0, last_class)
             feed_order = '-'.join(feed_order)
             FeedOrderModel.add_feed_order(order=feed_order, count=1)
+
+            db.session.commit()
+
+            return make_response(jsonify({
+                "success": True,
+                "data": {
+                    "feed_order": feed_order,
+                    "message": "새로운 급식 순서 생성"
+                }
+            }), 201)
         else:
             feed_order.count += 1
-        db.session.commit()
 
-    return "meal_order_update"
+            db.session.commit()
+
+            return make_response(jsonify({
+                "success": True,
+                "data": {
+                    "feed_order":feed_order.order,
+                    "message": "급식 순서 카운팅"
+                }
+            }), 200)
+
+    return make_response(jsonify({
+        "success": True,
+        "data": {
+            "message": "당일 급식 조회 실패"
+        }
+    }))
 
 
 @meal_order.route('/get/')
 def get_meal_order():
-    return jsonify({
+    return make_response(jsonify({
         "success": True,
         "data": {
             "feed_order": FeedOrderModel.latest_feed_order().order
         }
-    })
+    }), 200)
