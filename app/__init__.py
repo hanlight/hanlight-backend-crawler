3e1496dfb376ba4f9379d63c7a0923885c28af6a
leensungzero <brianwoo119@naver.com>
2019-05-27 11:17:48 +0900

    MealOrder 스케줄러 생성
    app/api/controllers/meal_order.py
     - DEL: response 제거
    
    app/__init__.py
     - update_meal_order 함수를 Background에서 하루마다 실행시키는 스케줄러 생성 및 시작
    
    server.py
     - use_reloader 비활성화

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 app = Flask(__name__)
 
@@ -23,3 +25,11 @@ from app.api.controllers.meal_order import meal_order
 
 
 app.register_blueprint(meal_order)
+
+
+from app.api.controllers import meal_order as m_o
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+scheduler.add_job(m_o.update_meal_order, 'cron', day='*/1')
